8a934f2cb5c9631fd3421fabfc098df4c54b606a
MatricalDefunkt <pramitsmusic@gmail.com>
2022-05-22 05:38:33 +0530

    Exported warn command

--- src/CommandsSlash/exports.ts
+++ src/CommandsSlash/exports.ts
@@ -6,6 +6,16 @@ import { Translate } from "./translate";
 import { Prefix } from "./prefix";
 import { Note } from "./note";
 import { Logs } from "./logs";
+import { Warn } from "./warn";
+import { Kick } from "./kick";
 
-export const SlashCommands: Command[] = [Slow, Translate, Prefix, Note, Logs];
+export const SlashCommands: Command[] = [
+	Slow,
+	Translate,
+	Prefix,
+	Note,
+	Logs,
+	Warn,
+	Kick,
+];
 export const AutoCompleteCommands: AutoCompleteCommand[] = [Translate];
